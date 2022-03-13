1de3b4379e6f74b8f48104655d4a8c144fd9af18
Amaury <amaury.lapaque@gmail.com>
2022-03-13 13:29:09 +0100

    play pause and volume works

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,34 +1,34 @@
 import {
-  HeartIcon,
   HomeIcon,
   LibraryIcon,
   PlusCircleIcon,
   RssIcon,
-  SearchIcon
-} from '@heroicons/react/outline';
-import { useSession } from 'next-auth/react';
-import { useEffect, useState } from 'react';
-import { useRecoilState } from 'recoil';
-import { playlistIdState } from '../atoms/playlistAtom';
-import useSpotify from '../hooks/useSpotify';
+  SearchIcon,
+} from '@heroicons/react/outline'
+import { HeartIcon } from '@heroicons/react/solid'
+import { useSession } from 'next-auth/react'
+import { useEffect, useState } from 'react'
+import { useRecoilState } from 'recoil'
+import { playlistIdState } from '../atoms/playlistAtom'
+import useSpotify from '../hooks/useSpotify'
 
 const Sidebar = () => {
-  const spotifyApi = useSpotify();
-  const { data: session, status } = useSession();
-  const [ playlist, setPlaylist ] = useState<Array<any>>([]);
-  const [ playlistId, setPlaylistId ] = useRecoilState(playlistIdState);
+  const spotifyApi = useSpotify()
+  const { data: session, status } = useSession()
+  const [playlist, setPlaylist] = useState<Array<any>>([])
+  const [playlistId, setPlaylistId] = useRecoilState(playlistIdState)
 
   useEffect(() => {
     if (spotifyApi.getAccessToken()) {
       spotifyApi.getUserPlaylists({ limit: 50 }).then((data) => {
-        setPlaylist(data.body.items);
-        setPlaylistId((data.body.items as any)[0]?.id);
-      });
+        setPlaylist(data.body.items)
+        setPlaylistId((data.body.items as any)[0]?.id)
+      })
     }
-  }, [ session, spotifyApi ]);
+  }, [session, spotifyApi])
 
   return (
-    <div className="sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide">
+    <div className="hidden h-screen overflow-y-scroll border-r border-gray-900 p-5 text-xs text-gray-500 scrollbar-hide sm:max-w-[12rem] md:inline-flex lg:max-w-[15rem] lg:text-sm">
       <div className="space-y-4">
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
@@ -50,29 +50,29 @@ const Sidebar = () => {
           <p>Create playlist</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <HeartIcon className="h-5 w-5" />
+          <HeartIcon className="h-5 w-5 text-blue-500" />
           <p>Liked songs</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <RssIcon className="h-5 w-5" />
+          <RssIcon className="h-5 w-5 text-green-500" />
           <p>Your episodes</p>
         </button>
 
-
         <hr className="border-t-[0.1px] border-gray-900" />
 
-        {/* PLAYLIST */ }
-        { playlist.map((playlist) => (
+        {/* PLAYLIST */}
+        {playlist.map((playlist) => (
           <p
-            key={ playlist.id }
+            key={playlist.id}
             className="cursor-pointer hover:text-white"
-            onClick={ () => setPlaylistId(playlist.id) }>
-            { playlist.name }
+            onClick={() => setPlaylistId(playlist.id)}
+          >
+            {playlist.name}
           </p>
-        )) }
+        ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Sidebar;
+export default Sidebar
